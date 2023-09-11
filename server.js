779cb65189cc8eedcde10eb1f67bb82e56bcc16b
Eduardo <miltormentas@tutanota.com>
2023-09-10 18:17:58 -0700

    better request info

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ serve({
     port: 8080,
     fetch(req) {
         const url = new URL(req.url);
-        if (url.pathname === "/") return new Response(Bun.file("./index.html"));
+        if (url.pathname === "/") {
+            // Log additional details about the request
+            console.log('Browser: ', req.headers.get('user-agent'));
+            console.log('Timestamp: ', new Date().toISOString());
+            // Serve the index.html file
+            return new Response(Bun.file("./index.html"));
+        }
+
         if (url.pathname === "/randomvideo") {
             const videoDir = './videos'
             const files = fs.readdirSync(videoDir); // Synchronous read, use fs.readdir for async
